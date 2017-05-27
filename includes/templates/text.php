ee338b71e87dddb7d75b9f78f860b94f75824e82
Alex Bondarev <skip405@gmail.com>
2017-05-27 20:01:09 +0300

    HTML templates structure tweaked

--- includes/templates/text.php
+++ includes/templates/text.php
@@ -1,4 +1,4 @@
-<div class="alchemy__field field field--{{TYPE}}" id="field--{{ID}}">
+<div class="alchemy__field field field--{{TYPE}}" id="field--{{ID}}" data-alchemy='{"id":"{{ID}}","type":"{{TYPE}}"}'>
     <label class="field__label" for="{{ID}}">{{TITLE}}</label>
     <input {{ATTRIBUTES}} />
     <div class="field__description">
--- includes/templates/textarea.php
+++ includes/templates/textarea.php
@@ -1,4 +1,4 @@
-<div class="alchemy__field field field--{{TYPE}}" id="field--{{ID}}">
+<div class="alchemy__field field field--{{TYPE}}" id="field--{{ID}}" data-alchemy='{"id":"{{ID}}","type":"{{TYPE}}"}'>
     <label class="field__label" for="{{ID}}">{{TITLE}}</label>
     <textarea {{ATTRIBUTES}}>{{VALUE}}</textarea>
     <div class="field__description">
