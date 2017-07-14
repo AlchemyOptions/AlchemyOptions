db0d946078c2728a04216829cb425ae7cf611263
Alex Bondarev <alexander.bondarev@dotmailer.com>
2017-07-14 17:43:16 +0300

    Sections field done

--- assets/scripts/methods/sections.js
+++ assets/scripts/methods/sections.js
@@ -2,6 +2,21 @@ export default function (scope = document) {
     const $sections = $('.jsAlchemySectionsFiled', scope);
 
     if( $sections[0] ) {
-        console.log(2);
+        $sections.each((i, el) => {
+            const $section = $(el);
+            const $tabs = $section.children('.jsAlchemySectionsTabs').children('.jsAlchemySectionsTab');
+            const $tabBtns = $section.children('.jsAlchemySectionsNav').children('.jsAlchemySectionsButton');
+
+            $section.on('click', '.jsAlchemySectionsButton', function() {
+                const $btn = $(this);
+
+                $tabBtns.removeClass('field__tab-btn--active');
+                $btn.addClass('field__tab-btn--active');
+
+                $tabs.hide().filter((i, el) => {
+                    return $(el).data('controlled-by') === $btn.data('controls')
+                }).show();
+            });
+        });
     }
 }
--- includes/fields/class.alchemySections.php
+++ includes/fields/class.alchemySections.php
@@ -11,7 +11,7 @@ if( ! class_exists( 'Alchemy_Sections_Field' ) ) {
             parent::__construct( $networkField );
 
             $this->template = '
-                <div class="alchemy__field field field--{{TYPE}} jsAlchemySectionsFiled" data-alchemy=\'{"type":"{{TYPE}}"}\'>
+                <div class="alchemy__field field field--{{TYPE}} clearfix jsAlchemySectionsFiled" data-alchemy=\'{"type":"{{TYPE}}"}\'>
                 {{FIELDS}}
                 </div>
             ';
@@ -28,20 +28,31 @@ if( ! class_exists( 'Alchemy_Sections_Field' ) ) {
             $sections = isset( $field['sections'] ) ? $field['sections'] : array();
 
             if( is_array( $sections ) && count( $sections ) > 0 ) {
-                $navHTML = '<div class="jsAlchemySectionsNav">';
-                $tabsHTML = '<div class="jsAlchemySectionsTabs">';
+                $navHTML = '<div class="field__tabs-nav jsAlchemySectionsNav">';
+                $tabsHTML = '<div class="field__tabs jsAlchemySectionsTabs">';
 
-                foreach ( $sections as $section ) {
+                foreach ( $sections as $i => $section ) {
+                    $btnClass = 'field__tab-btn jsAlchemySectionsButton';
+                    $tabClass = 'field__tab jsAlchemySectionsTab';
+
+                    if( $i === 0 ) {
+                        $btnClass .= ' field__tab-btn--active';
+                    } else {
+                        $tabClass .= ' field__tab--hidden';
+                    }
+
                     $navHTML .= sprintf(
-                        '<button type="button" data-controls="%2$s">%1$s</button>',
+                        '<button type="button" class="%3$s" data-controls="%2$s">%1$s</button>',
                             $section['title'],
-                            $this->make_label( $section['title'] )
+                            $this->make_label( $section['title'] ),
+                            $btnClass
                     );
 
                     if( is_array( $section['options'] ) && count( $section['options'] ) > 0 ) {
                         $tabsHTML .= sprintf(
-                            '<div class="jsAlchemySectionsTab" data-controlled-by="%s">',
-                                $this->make_label( $section['title'] )
+                            '<div class="%2$s" data-controlled-by="%1$s">',
+                                $this->make_label( $section['title'] ),
+                                $tabClass
                         );
 
                         $sectionFields = new Alchemy_Fields_Loader( $this->networkField );
