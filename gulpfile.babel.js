aae4d098f3fa48f18a8eb6431c04de9b36f32435
Alex Bondarev <skip405@gmail.com>
2018-01-27 22:19:23 +0300

    Build process done

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,15 @@
 'use strict';
 
-const productionBuild = process.env.NODE_ENV === 'production';
+const argv = require('yargs').argv;
+
+if( 'build' === argv._[0] && ! argv.ver ) {
+    throw new Error('--ver param is required for the build task');
+}
+
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
 const gulp = require('gulp');
 const pump = require('pump');
 const del = require('del');
@@ -9,11 +18,89 @@ const plugins = require('gulp-load-plugins')({
         'gulp-rev-replace': 'revReplace'
     }
 });
-const pathToBuild = 'build';
+const productionBuild = process.env.NODE_ENV === 'production';
+const pathToBuild = 'dist';
+const destinationFolder = productionBuild
+    ? path.resolve(__dirname, `./${pathToBuild}/assets/scripts`)
+    : path.resolve(__dirname, './assets/scripts');
+const webpackConfig = {
+    entry: {
+        'alchemy': './alchemy.js',
+        'alchemy-client': './alchemy-client.js'
+    },
+    context: path.resolve(__dirname, './assets/scripts'),
+    output: {
+        path: destinationFolder,
+        filename: '[name].min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    performance: {
+        hints: false
+    },
+    resolve: {
+        extensions: [".js", ".es6"]
+    },
+    externals: /^(jQuery|\$|tinymce)$/i,
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            sourceMap: true
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jQuery',
+            jQuery: 'jQuery'
+        })
+    ],
+    watch: ! productionBuild
+};
+const replaceInFiles = [
+    `./${pathToBuild}/includes/class-options-loader.php`,
+    `./${pathToBuild}/includes/class-meta-box.php`
+];
+const sourceFilesToDelete = [
+    `./${pathToBuild}/assets/styles/rev-manifest.json`,
+    `./${pathToBuild}/assets/scripts/rev-manifest.json`,
+    `./${pathToBuild}/assets/scripts/alchemy.min.js`,
+    `./${pathToBuild}/assets/scripts/alchemy-client.min.js`,
+    `./${pathToBuild}/assets/scripts/alchemy.min.js.map`,
+    `./${pathToBuild}/assets/scripts/alchemy-client.min.js.map`
+];
+const alchemyVersion = argv.ver;
+
+if (productionBuild) {
+    webpackConfig.devtool = '#source-map';
+}
 
 gulp.task('clean', cb => {
     del([
         `./${pathToBuild}/**`
+    ]).then(() => {
+        cb();
+    });
+});
+
+gulp.task('clean:source', cb => {
+    del(sourceFilesToDelete).then(() => {
+        cb();
+    });
+});
+
+gulp.task('clean:build', cb => {
+    del([
+        `./${pathToBuild}/**`,
+        `!./${pathToBuild}`,
+        `!./${pathToBuild}/alchemy-options.zip`,
+        `!./${pathToBuild}/VERSION`
     ]).then(() => {
         cb();
     });
@@ -24,6 +111,7 @@ gulp.task('copy', cb => {
         gulp.src([
             `./includes/**/*`,
             `./languages/**/*`,
+            `./assets/vendor/**/*`,
             `./alchemy-options.php`,
             `./autoload.php`,
             `./index.php`,
@@ -31,6 +119,17 @@ gulp.task('copy', cb => {
             `LICENSE`
         ], { base: `.` }),
         gulp.dest(`./${pathToBuild}/`)
+    ], cb);
+});
+
+gulp.task('scripts', cb => {
+    pump([
+        webpackStream(webpackConfig),
+        gulp.dest(destinationFolder),
+        plugins.rev(),
+        gulp.dest(destinationFolder),
+        plugins.rev.manifest(),
+        gulp.dest(destinationFolder)
     ], cb);
 });
 
@@ -47,6 +146,16 @@ gulp.task('styles:main', cb => {
     ], cb);
 });
 
+gulp.task('styles:main-watch', cb => {
+    pump([
+        gulp.src('./assets/styles/alchemy.scss'),
+        plugins.sass({
+            outputStyle: 'compressed'
+        }),
+        gulp.dest(`./assets/styles/`)
+    ], cb);
+});
+
 gulp.task('styles', ['styles:main'], cb => {
     pump([
         gulp.src('./assets/vendor/select2/css/select2.css'),
@@ -58,57 +167,86 @@ gulp.task('styles', ['styles:main'], cb => {
     ], cb);
 });
 
-gulp.task('unhash', cb => {
+gulp.task('styles:watch', () => {
+    gulp.watch('./assets/styles/**/*.scss', ['styles:main-watch']);
+});
+
+gulp.task('version', cb => {
     pump([
-        gulp.src(`./${pathToBuild}/inc/script-style.php`),
-        plugins.replace(new RegExp(`${project}-(.*?).css`, "i"), `${project}.css`),
-        plugins.replace(new RegExp(`${project}-(.*?).min.js`, "i"), `${project}.min.js`),
-        plugins.replace(new RegExp(`vendor-(.*?).min.js`, "i"), `vendor.min.js`),
-        gulp.dest(`./${pathToBuild}/inc/`)
+        gulp.src(`${pathToBuild}/alchemy-options.php`),
+        plugins.replace('0.0.1', alchemyVersion),
+        gulp.dest(`./${pathToBuild}/`)
     ], cb)
 });
 
-gulp.task('hash:scripts', cb => {
+gulp.task('add-version', cb => {
+    fs.writeFile(`./${pathToBuild}/VERSION`, alchemyVersion, 'utf8', err => {
+        if (err) throw err;
+
+        cb();
+    });
+});
+
+gulp.task('archive', cb => {
     pump([
-        gulp.src([
-            `${pathToSource}/scripts/${project}.min.js`,
-            `${pathToSource}/scripts/vendor.min.js`
-        ]),
-        plugins.rev(),
-        gulp.dest(`./${pathToBuild}/scripts/`),
-        plugins.rev.manifest(),
-        gulp.dest(`./${pathToBuild}/scripts/`)
+        gulp.src(`${pathToBuild}/**/*`),
+        plugins.zip('alchemy-options.zip'),
+        gulp.dest(pathToBuild)
     ], cb);
 });
 
 gulp.task('revreplace:styles', cb => {
-    const manifest = gulp.src(`./${pathToBuild}/styles/rev-manifest.json`);
+    const manifest = gulp.src(`./${pathToBuild}/assets/styles/rev-manifest.json`);
 
     pump([
-        gulp.src(`./${pathToBuild}/inc/script-style.php`),
+        gulp.src(replaceInFiles),
         plugins.revReplace({
             replaceInExtensions: ['.php'],
             manifest: manifest
         }),
-        gulp.dest(`./${pathToBuild}/inc`)
+        gulp.dest(`./${pathToBuild}/includes`)
     ], cb);
 });
 
 gulp.task('revreplace:scripts', cb => {
-    const manifest = gulp.src(`./${pathToBuild}/scripts/rev-manifest.json`);
+    const manifest = gulp.src(`./${pathToBuild}/assets/scripts/rev-manifest.json`);
 
     pump([
-        gulp.src(`./${pathToBuild}/inc/script-style.php`),
+        gulp.src(replaceInFiles),
         plugins.revReplace({
-            replaceInExtensions: ['.php'],
+            replaceInExtensions: ['.php', '.js'],
             manifest: manifest
         }),
-        gulp.dest(`./${pathToBuild}/inc`)
+        gulp.dest(`./${pathToBuild}/includes`)
     ], cb);
 });
+
+gulp.task('revreplace:maps', cb => {
+    const manifest = gulp.src(`./${pathToBuild}/assets/scripts/rev-manifest.json`);
+
+    pump([
+        gulp.src(`${pathToBuild}/assets/scripts/**/*.min.js`),
+        plugins.revReplace({
+            replaceInExtensions: ['.js'],
+            manifest: manifest
+        }),
+        gulp.dest(`./${pathToBuild}/assets/scripts`)
+    ], cb);
+});
+
+gulp.task('default', plugins.sequence(
+    'styles:watch',
+    'scripts'
+));
 
 gulp.task('build', plugins.sequence(
     ['clean'],
     'copy',
-    'styles'
+    ['styles', 'scripts', 'version'],
+    ['revreplace:styles', 'revreplace:maps'],
+    'revreplace:scripts',
+    'clean:source',
+    'archive',
+    'add-version',
+    'clean:build'
 ));
