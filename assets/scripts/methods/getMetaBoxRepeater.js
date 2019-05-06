dc7da25f1aec14cbfaa79007e93162437ff8e8f8
skip405 <skip405@gmail.com>
2019-05-06 17:37:58 +0300

    MetaBox repeaters get saved in WP v5

--- assets/scripts/methods/getMetaBoxRepeater.js
+++ assets/scripts/methods/getMetaBoxRepeater.js
@@ -40,5 +40,37 @@ export default function() {
                 e.preventDefault();
             });
         }
+    } else if( wp.data.subscribe ) {
+        const $metaBoxes = $('.jsAlchemyMetaBox');
+
+        if( $metaBoxes[0] ) {
+            wp.data.subscribe(() => {
+                const isSavingPost = wp.data.select('core/editor').isSavingPost();
+                const isAutosavingPost = wp.data.select('core/editor').isAutosavingPost();
+
+                if ( isSavingPost && ! isAutosavingPost ) {
+                    $metaBoxes.each((i, metabox) => {
+                        const $metaBox = $(metabox);
+                        const $sections = $metaBox.children('.jsAlchemySectionsFiled');
+
+                        const $repeaters = $sections[0]
+                            ? $sections.children('.jsAlchemySectionsTabs').children('.jsAlchemySectionsTab').children('.jsAlchemyRepeaterField')
+                            : $metaBox.children('.jsAlchemyRepeaterField');
+
+                        if( $repeaters[0] ) {
+                            $repeaters.each((i, repeater) => {
+                                const $repeater = $(repeater);
+                                let value = getFieldValue($repeater, true);
+
+                                value = JSON.stringify(value);
+                                value = value.replace(/\\\"/g, '&#34;'); // hack to replace escaped double quotes in JSON
+
+                                $repeater.children('fieldset').children('.jsRepeaterHidden').val(value);
+                            });
+                        }
+                    });
+                }
+            })
+        }
     }
 }
